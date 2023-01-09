7b65ec479682f9fce3c496e819fe6ea3037849de
Hanjin Lee <jinifor@gmail.com>
2023-01-09 19:48:36 +0900

    Add use Users resource 자동생성
    - controller
    - service
    - entity
    - dto
    - test code

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 /**
  * 애플리케이션의 루트 모듈 : The root module of the application.
  */
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
