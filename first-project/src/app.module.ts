20f59cc12aa0fafbbaec6c5c66aae7bafb8c7a18
Hanjin Lee <jinifor@gmail.com>
2023-01-10 21:18:15 +0900

    Add use Managers Controller

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { ManagersController } from './managers/managers.controller';
 
 /**
  * 애플리케이션의 루트 모듈 : The root module of the application.
  */
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
+  controllers: [AppController, UsersController, ManagersController],
   providers: [AppService],
 })
 export class AppModule {}
