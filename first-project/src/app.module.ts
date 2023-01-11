2107256921c47fbbe9a7bb6edb4e0a8764c85b47
Hanjin Lee <jinifor@gmail.com>
2023-01-11 18:58:11 +0900

    manager controller remove

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -3,14 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
-import { ManagersController } from './managers/managers.controller';
 
 /**
  * 애플리케이션의 루트 모듈 : The root module of the application.
  */
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController, ManagersController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
